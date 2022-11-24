939c0e5528139730a036f5314c4b02d8655bf925
Maarten Hogeveen <mhogeveen94@gmail.com>
2022-11-24 19:33:11 +0100

    Remove unused functionality

--- lua/reach/tabpages/make_tabpages.lua
+++
@@ -1,7 +0,0 @@
-local Tabpage = require('reach.tabpages.tabpage')
-
-return function(_)
-  return vim.tbl_map(function(tabnr)
-    return Tabpage:new(tabnr)
-  end, vim.api.nvim_list_tabpages())
-end
